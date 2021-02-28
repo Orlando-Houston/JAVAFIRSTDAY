f4f6be0b6395fde8e992f451a0e18cd40ab55476
Altun <a.ozder@outlook.com>
2021-02-28 17:09:26 -0500

    JavaDay_7

---
+++ src/Day_7/TernaryOperator.java
@@ -0,0 +1,8 @@
+package Day_7;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        String answer = ("a".equalsIgnoreCase("A"))? "OK" : "NOT OK";
+        System.out.println(answer);
+    }
+}
