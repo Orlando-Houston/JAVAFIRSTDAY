73c6c80cacc1d26e94d291f7a990f0856b8caca3
Altun <a.ozder@outlook.com>
2021-04-07 10:18:22 -0400

    Day_16_7_8

---
+++ src/Day_16_7_8/First.java
@@ -0,0 +1,14 @@
+package Day_16_7_8;
+
+public class First {
+    public static void main(String[] args) {
+        String[] cities = {"New York","Seattle","San Diego","Boulder","Ann Arbor"};
+
+        for (int i=0 ; i<5 ; i++){
+            System.out.println(cities[i]);
+        }
+        for(int i=cities.length-1;i>=0;i--){
+            System.out.println(cities[i]);
+        }
+    }
+}
---
+++ src/Day_16_7_8/StringArrayLength.java
@@ -0,0 +1,16 @@
+package Day_16_7_8;
+
+public class StringArrayLength {
+    public static void main(String[] args) {
+        String[] a={"abc","defgh","qwerty","asd"};
+        int counter=0;
+        for(int i=0 ; i<a.length;i++){
+            if(a[i].length()<4){
+                counter++;
+                System.out.println(a[i]);
+            }
+        }
+
+        System.out.println(counter);
+    }
+}
