24628f4d804582c40ee9c41f3294d0372aa41d05
Altun <a.ozder@outlook.com>
2021-04-08 11:03:36 -0400

    Day_21_Array

---
+++ src/Dat_21_Array/RangeCalculation.java
@@ -0,0 +1,7 @@
+package Dat_21_Array;
+
+public class RangeCalculation {
+    public static int calculateRange(double mpg, double tank){
+        return (int)(mpg*tank);
+    }
+}
---
+++ src/Dat_21_Array/RangeCalculationTest.java
@@ -0,0 +1,7 @@
+package Dat_21_Array;
+
+public class RangeCalculationTest {
+    public static void main(String[] args) {
+        System.out.println (RangeCalculation.calculateRange (29.6, 9.7));
+    }
+}
