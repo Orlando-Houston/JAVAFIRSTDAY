6d695064e7096fe61738b176404e60fa69e4f43b
Altun <a.ozder@outlook.com>
2021-04-14 15:40:38 -0400

    Day_23_29_CreatingClasses

---
+++ src/March_17_CreateClassess/CarInsuranceTest.java
@@ -0,0 +1,11 @@
+package March_17_CreateClassess;
+
+public class CarInsuranceTest {
+    public static void main(String[] args) {
+        CarInsurance myIns = new CarInsurance(2015,0,0);
+        double myOffer=myIns.makeOffer(2020);
+        System.out.println(myOffer);
+        CarInsurance second = new CarInsurance(2019,3,2);
+        System.out.println(second.makeOffer(2020));
+    }
+}
