ec2d3a5771f2123bdfe841204baddb953e1b197a
Altun <a.ozder@outlook.com>
2021-04-14 15:46:25 -0400

    Day_23_29_CreatingClasses

---
+++ src/March_18createClassess/InterviewTest.java
@@ -0,0 +1,11 @@
+package March_18createClassess;
+
+public class InterviewTest {
+    public static void main(String[] args) {
+        Interview candidate1 = new Interview("Nihal",100,100,100);
+        System.out.println(candidate1.toString());
+        System.out.println(candidate1.decideResult());
+        candidate1.calculateScore();
+        System.out.println(candidate1.decideResult());
+    }
+}
