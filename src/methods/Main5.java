b5d58b993f48222353f57263fab713ec3e82cba5
Altun <a.ozder@outlook.com>
2021-04-17 23:58:08 -0400

    methods

---
+++ src/methods/Main5.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Main5 {
+    public static void toplama(int a , int b ,int c) {
+
+        int  deneme = 10;
+        System.out.println("Toplama Sonucu: " + (a+b+c));
+
+    }
+    public static void main(String[] args) {
+        toplama(10,20,30);
+
+        System.out.println(deneme);
+
+    }
+}
